19d2a1d56138481c97eb540c8f077d6e26e37698
roris <roris@users.noreply.github.com>
2019-08-03 22:27:36 +0530

    feat: Add 'create new pair form'

--- packages/frontend/src/route-map.ts
+++ packages/frontend/src/route-map.ts
@@ -14,4 +14,10 @@ export const routeMap: RouteConfig[] = [
     title: 'Add new dog',
     moduleId: PLATFORM.moduleName('./components/dogs/new-dog')
   },
+  {
+    route: ['pairs/new'],
+    name: 'new-pair',
+    title: 'Create new pair',
+    moduleId: PLATFORM.moduleName('./components/pairs/new-pair')
+  }
 ];
