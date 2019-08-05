57a6706a34e84e6d5504a34a68221d4bbcf8cee9
roris <roris@users.noreply.github.com>
2019-08-05 19:21:43 +0530

    chore: Use eslint

---
+++ packages/backend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'promise'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:promise/recommended',
+    'standard',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  env: {
+    node: true
+  },
+  rules: {
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+    quotes: ['error', 'single']
+  }
+};
--- packages/backend/src/declarations.d.ts
+++ packages/backend/src/declarations.d.ts
@@ -3,6 +3,6 @@ import { Service } from '@feathersjs/feathers';
 import '@feathersjs/transport-commons';
 
 // A mapping of service names to types. Will be extended in service files.
-export interface ServiceTypes {}
+export interface ServiceTypes { }
 // The application instance type that will be used everywhere else
 export type Application = ExpressFeathers<ServiceTypes>;
