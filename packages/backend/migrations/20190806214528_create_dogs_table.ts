904ab81ff7ec8e4c703e02058291cbf721410151
roris <roris@users.noreply.github.com>
2019-08-06 22:03:08 +0530

    fix(migrations): create dogs table after breeds table

---
+++ packages/backend/migrations/20190806214528_create_dogs_table.ts
@@ -0,0 +1,54 @@
+import * as Knex from 'knex';
+
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable('dogs', table => {
+    table.increments('id')
+      .unsigned()
+      .primary()
+      .notNullable();
+    table.integer('microchipNo')
+      .unsigned()
+      .unique()
+      .index()
+      .nullable();
+    table.string('name')
+      .nullable();
+    table.string('gender', 1)
+      .notNullable();
+    table.date('dateOfBirth')
+      .nullable();
+    table.integer('owner')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('SET NULL');
+    table.integer('breeder')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('SET NULL');
+    table.integer('breed')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('breeds')
+      .onDelete('SET NULL');
+    table.integer('picture')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('uploads')
+      .onDelete('SET NULL');
+    table.timestamp('createdAt');
+    table.timestamp('updatedAt');
+  });
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable('dogs');
+}
+
