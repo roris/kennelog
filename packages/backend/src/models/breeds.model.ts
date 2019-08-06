1d4fbc6e3f1f056cb34d20dc516a4b732de90a38
roris <roris@users.noreply.github.com>
2019-08-06 23:12:30 +0530

    feat: Add breeds service

---
+++ packages/backend/src/models/breeds.model.ts
@@ -0,0 +1,55 @@
+// See https://vincit.github.io/objection.js/#models
+// for more of what you can do here.
+import { Model, JsonSchema, RelationMappings } from 'objection';
+import Knex from 'knex';
+import { Application } from '../declarations';
+
+class Breeds extends Model {
+  createdAt!: string;
+
+  updatedAt!: string;
+
+  name!: string;
+
+  static get tableName() {
+    return 'breeds';
+  }
+
+  static get jsonSchema(): JsonSchema {
+    return {
+      type: 'object',
+      required: ['name'],
+
+      properties: {
+        name: { type: 'string' }
+      }
+    };
+  }
+
+  static get relationMappings(): RelationMappings {
+    const Dogs = require('./dogs.model');
+
+    return {
+      dogs: {
+        relation: Model.HasManyRelation,
+        modelClass: Dogs,
+        join: {
+          from: 'breeds.id',
+          to: 'dogs.breed'
+        }
+      }
+    }
+  }
+
+  $beforeInsert() {
+    this.createdAt = this.updatedAt = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updatedAt = new Date().toISOString();
+  }
+}
+
+export default function (app: Application) {
+  return Breeds;
+}
--- packages/backend/src/services/index.ts
+++ packages/backend/src/services/index.ts
@@ -1,9 +1,11 @@
 import { Application } from '../declarations';
 import users from './users/users.service';
 import dogs from './dogs/dogs.service';
+import breeds from './breeds/breeds.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(users);
   app.configure(dogs);
+  app.configure(breeds);
 }
