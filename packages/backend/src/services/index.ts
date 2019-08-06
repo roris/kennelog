0682e6ff8f30f6e1e6c1a6474355c9130f2333d0
roris <roris@users.noreply.github.com>
2019-08-07 02:01:34 +0530

    feat: Add hip-scores service

--- packages/backend/src/services/index.ts
+++ packages/backend/src/services/index.ts
@@ -12,6 +12,7 @@ import measures from './measures/measures.service';
 import weights from './weights/weights.service';
 import heights from './heights/heights.service';
 import lengths from './lengths/lengths.service';
+import hipScores from './hip-scores/hip-scores.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
@@ -28,4 +29,5 @@ export default function (app: Application): void {
   app.configure(weights);
   app.configure(heights);
   app.configure(lengths);
+  app.configure(hipScores);
 }
