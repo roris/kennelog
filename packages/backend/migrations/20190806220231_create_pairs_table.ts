52e2ebd9de0fdbd9158d3860226ee4d8637aa795
roris <roris@users.noreply.github.com>
2019-08-06 22:07:09 +0530

    feat(migrations): add script for pairs table

---
+++ packages/backend/migrations/20190806220231_create_pairs_table.ts
@@ -0,0 +1,35 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable('pairs', table => {
+    table.increments('id')
+      .unsigned()
+      .primary()
+      .notNullable();
+    table.integer('sire')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('dogs');
+    table.integer('dame')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('dogs');
+    table.integer('pairedBy')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('users');
+    table.date('pairedOn')
+      .nullable();
+    table.timestamp('createdAt');
+    table.timestamp('updatedAt');
+  });
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+}
+
