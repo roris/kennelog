4ccda8e0ef0aa7a85c4c2e1ae1891ce3fe67697e
roris <roris@users.noreply.github.com>
2019-08-06 22:34:30 +0530

    feat(migrations): add script for creating measures table

---
+++ packages/backend/migrations/20190806222909_create_measures_table.ts
@@ -0,0 +1,27 @@
+import * as Knex from 'knex';
+
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable('measures', table => {
+    table.increments('id')
+      .unsigned()
+      .primary()
+      .notNullable();
+    table.date('measuredOn')
+      .notNullable();
+    table.integer('dog')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('dogs')
+      .onDelete('CASCADE');
+    table.timestamp('createdAt');
+    table.timestamp('updatedAt');
+  });
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable('measures');
+}
+
