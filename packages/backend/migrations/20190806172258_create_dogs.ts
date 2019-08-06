2ded65a22dfebd7acd979d8f0700346882f30c25
roris <roris@users.noreply.github.com>
2019-08-06 17:53:25 +0530

    feat(migrations): add script for creating dogs table

---
+++ packages/backend/migrations/20190806172258_create_dogs.ts
@@ -0,0 +1,40 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.hasTable('dogs').then(exists => {
+    if (!exists) {
+      knex.schema.createTable('dogs', table => {
+        table.increments('id')
+          .unsigned()
+          .notNullable();
+        table.integer('microchipNo')
+          .unsigned()
+          .unique()
+          .index()
+          .nullable();
+        table.string('name')
+          .nullable();
+        table.string('gender', 1)
+          .notNullable();
+        table.date('dateOfBirth')
+          .nullable();
+        table.timestamp('createdAt');
+        table.timestamp('updatedAt');
+      })
+        .then(() => console.log('Created dogs table')); // eslint-disable-line no-console
+    }
+  })
+    .catch(e => console.error('Error creating dogs table', e));
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.hasTable('dogs').then(exists => {
+    if (exists) {
+      knex.schema.dropTable('dogs')
+        .then(() => console.log('Dropped table dogs'));  // eslint-disable-line no-console
+    }
+  })
+    .catch(e => console.error('Error dropping dogs table', e));  // eslint-disable-line no-console
+}
+
