cf928f9329d661ef5b216fba0f1eb5bb4f96f4cf
roris <roris@users.noreply.github.com>
2019-08-06 10:18:05 +0530

    feat: Enable CORS

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -26,6 +26,11 @@ app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
 app.use(helmet());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
