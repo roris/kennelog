c55dcc695458e6f0d8764398f80b8c101c063d5a
roris <roris@users.noreply.github.com>
2019-08-08 22:58:17 +0530

    refactor(blobs): change the storage directory

--- packages/backend/src/services/blobs/blobs.service.ts
+++ packages/backend/src/services/blobs/blobs.service.ts
@@ -3,22 +3,21 @@ import { ServiceAddons } from '@feathersjs/feathers';
 import { Application } from '../../declarations';
 import hooks from './blobs.hooks';
 
-import {Service as BlobService} from 'feathers-blob';
+import { Service as BlobService } from 'feathers-blob';
 import fs from 'fs-blob-store';
 
 // Add this service to the service type index
 declare module '../../declarations' {
   interface ServiceTypes {
-    'blobs': BlobService & ServiceAddons<any>;
+    blobs: BlobService & ServiceAddons<any>;
   }
 }
 
-const blobStorage = fs('./content');
+const blobStorage = fs('./public/uploads');
 
-export default function (app: Application) {
-
+export default function(app: Application) {
   // Initialize our service with any options it requires
-  app.use('/blobs', new BlobService({ Model: blobStorage}));
+  app.use('/blobs', new BlobService({ Model: blobStorage }));
 
   // Get our initialized service so that we can register hooks
   const service = app.service('blobs');
