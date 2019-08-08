0964c193a02d028b444e58fd678b25fb7c87e748
roris <roris@users.noreply.github.com>
2019-08-08 16:58:48 +0530

    feat(uploads): increase uri column length to 500000

--- packages/backend/migrations/20190806213941_create_uploads_table.ts
+++ packages/backend/migrations/20190806213941_create_uploads_table.ts
@@ -1,13 +1,14 @@
 import * as Knex from 'knex';
 
-
 export async function up(knex: Knex): Promise<any> {
   return knex.schema.createTable('uploads', table => {
-    table.increments('id')
+    table
+      .increments('id')
       .unsigned()
       .primary()
       .notNullable();
-    table.string('uri')
+    table
+      .string('uri', 500000)
       .unique()
       .index()
       .notNullable();
@@ -16,8 +17,6 @@ export async function up(knex: Knex): Promise<any> {
   });
 }
 
-
 export async function down(knex: Knex): Promise<any> {
   return knex.schema.dropTable('uploads');
 }
-
