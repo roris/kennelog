2857168abf8a8aebc5653d7ac9b77ab0882296e1
roris <roris@users.noreply.github.com>
2019-08-09 03:54:49 +0530

    fix(dogs): fix empty gender written to db

--- packages/backend/src/hooks/sanitize-dog.ts
+++ packages/backend/src/hooks/sanitize-dog.ts
@@ -2,35 +2,37 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers';
 
+const sanitizeGender = (gender: string) => {
+  if (gender == '') {
+    throw new Error('gender is required');
+  }
+
+  gender = gender.toUpperCase();
+
+  if (gender !== 'M' && gender !== 'F') {
+    throw new Error('Invalid gender');
+  }
+
+  return gender;
+};
+
 export default (options = {}): Hook => {
   return async (context: HookContext) => {
     const data: any = {};
 
     // gender is the only required property
-    if (context.data.gender === undefined) {
-      throw new Error('gender is required');
-    }
+    data.gender = sanitizeGender(context.data.gender);
 
-    data.gender = context.data.gender;
-
-    if (context.data.name !== undefined) {
+    if (context.data.name != '') {
       data.name = context.data.name;
     }
 
-    if (context.data.microchipNo !== undefined) {
+    if (context.data.microchipNo != '') {
       data.microchipNo = context.data.microchipNo;
     }
 
-    if (context.data.dateOfBirth !== undefined) {
+    if (context.data.dateOfBirth != '') {
       data.dateOfBirth = context.data.dateOfBirth;
-    }
-
-    if (context.data.sire) {
-      data.sire = context.data.sire;
-    }
-
-    if (context.data.dame) {
-      data.dame = context.data.dame;
     }
 
     if (context.data.breed) {
