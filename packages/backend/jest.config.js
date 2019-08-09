c1880b4856a7e6adafa53ccd301a41c8053e2fc6
roris <roris@users.noreply.github.com>
2019-08-10 03:28:55 +0530

    chore: move jest configuration out of

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!**/*.test.{js,ts}',
+    '!**/node_modules/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: '<rootDir>/test/coverage',
+  coverageReporters: ['lcov'],
   globals: {
     'ts-jest': {
       diagnostics: false
     }
-  }
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '\\.test\\.(ts|js)$'
 };
