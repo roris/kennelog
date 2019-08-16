c228f720c41ffe21967819f20222d434fff1f567
roris <roris@users.noreply.github.com>
2019-08-16 22:57:30 +0530

    fix(seeds/pairs): create pair using the dogs' owner

--- packages/backend/seeds/004-pairs.ts
+++ packages/backend/seeds/004-pairs.ts
@@ -1,24 +1,26 @@
 import * as Knex from 'knex';
+import { getUserByEmail } from './util/common';
+
+const getEldestDogByOwnerAndGender = async (
+  knex: Knex,
+  ownerId: number,
+  gender: string
+) => {
+  const dogs = await knex
+    .select()
+    .from('dogs')
+    .where({ owner: ownerId })
+    .andWhere({ gender: gender })
+    .orderBy('dateOfBirth', 'asc')
+    .limit(1);
+  return dogs[0];
+};
 
 // creates one pair
 export async function seed(knex: Knex): Promise<any> {
-  const sires = await knex
-    .select()
-    .from('dogs')
-    .where({ gender: 'M' })
-    .limit(1);
-
-  const dames = await knex
-    .select()
-    .from('dogs')
-    .where({ gender: 'F' })
-    .limit(1);
-
-  const users = await knex
-    .select('id')
-    .from('users')
-    .where({ email: 'johnsmith@email.local' })
-    .limit(1);
+  const owner = await getUserByEmail(knex, 'johnsmith@email.local');
+  const sire = await getEldestDogByOwnerAndGender(knex, owner.id, 'M');
+  const dame = await getEldestDogByOwnerAndGender(knex, owner.id, 'F');
 
   // Deletes ALL existing entries
   return knex('pairs')
@@ -26,10 +28,10 @@ export async function seed(knex: Knex): Promise<any> {
     .then(() => {
       // Inserts seed entries
       return knex('pairs').insert({
-        dame: dames[0].id,
+        dame: dame.id,
         pairedOn: new Date().toISOString().substr(0, 10),
-        pairedBy: users[0].id,
-        sire: sires[0].id
+        pairedBy: owner.id,
+        sire: sire.id
       });
     });
 }
