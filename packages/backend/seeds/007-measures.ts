83fc9ed57a2b920979bc053b94bbfcd73eb85e1f
roris <roris@users.noreply.github.com>
2019-08-17 01:04:40 +0530

    fix(seeds): measurements' subtable records not being deleted [needs proper fix]

--- packages/backend/seeds/007-measures.ts
+++ packages/backend/seeds/007-measures.ts
@@ -2,5 +2,10 @@ import * as Knex from 'knex';
 
 export async function seed(knex: Knex): Promise<any> {
   // Deletes ALL existing entries
-  return knex('measures').del();
+  await knex('measures').del();
+  await knex('heights').del();
+  await knex('weights').del();
+  await knex('lengths').del();
+  await knex('hip_scores').del();
+  return knex('elbow_scores').del();
 }
