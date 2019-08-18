5c31742abeaea724eebbb0fcb153167c537ff0fe
roris <roris@users.noreply.github.com>
2019-08-18 23:00:59 +0530

    test(sanitize-dog): add more test cases

--- packages/backend/test/hooks/sanitize-dog.test.ts
+++ packages/backend/test/hooks/sanitize-dog.test.ts
@@ -1,6 +1,17 @@
 import feathers, { Application } from '@feathersjs/feathers';
 import sanitizeDog from '../../src/hooks/sanitize-dog';
 
+const params = {
+  gender: 'M',
+  name: 'Doge',
+  microchipNo: '123456789012345',
+  dateOfBirth: '2018-01-01',
+  breed: 1,
+  picture: 2,
+  breeder: 1,
+  owner: 1
+};
+
 describe("'sanitize-dog' hook", () => {
   let app: Application;
 
@@ -20,12 +31,38 @@ describe("'sanitize-dog' hook", () => {
     });
   });
 
-  it('Throws on empty data', async () => {
+  it('throws when no gender is given', async () => {
     expect.assertions(1);
     try {
       await app.service('dummy').create({});
     } catch (error) {
       expect(error).toBeDefined();
     }
+  });
+
+  it("throws when gender is neither 'M' nor 'F'", async () => {
+    expect.assertions(1);
+
+    try {
+      await app.service('dummy').create({ gender: 'A' });
+    } catch (error) {
+      expect(error.message).toBe('Invalid gender');
+    }
+  });
+
+  it('accepts requests which specify gender only', async () => {
+    expect.assertions(1);
+    const params = { gender: 'F' };
+
+    const result = await app.service('dummy').create(params);
+    expect(result).toEqual(params);
+  });
+
+  it('strips extra properties', async () => {
+    expect.assertions(1);
+    const withExtras = { ...params, extra: true, stripMe: false };
+
+    const result = await app.service('dummy').create(withExtras);
+    expect(result).toEqual(params);
   });
 });
