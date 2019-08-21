d817ad8ca89a865a3db255800a3105a93cbe31ff
roris <roris@users.noreply.github.com>
2019-08-21 10:04:23 +0530

    refactor(route-map): use consts to hold the moduleName

--- packages/frontend/src/route-map.ts
+++ packages/frontend/src/route-map.ts
@@ -1,151 +1,156 @@
 import { RouteConfig } from 'aurelia-router';
 import { PLATFORM } from 'aurelia-pal';
 
-const userLittersList = PLATFORM.moduleName(
-  './components/user-litters-list/user-litters-list'
+const NewDogForm = PLATFORM.moduleName('./components/new-dog/new-dog');
+const NewLitterForm = PLATFORM.moduleName('./components/new-litter/new-litter');
+const NewLitterFromPairForm = PLATFORM.moduleName(
+  './components/new-litter-from-pair/new-litter-from-pair'
 );
-
-const userDogsList = PLATFORM.moduleName(
+const NewPairForm = PLATFORM.moduleName('./components/new-pair/new-pair');
+const SignInForm = PLATFORM.moduleName('./components/sign-in/sign-in');
+const SignOutForm = PLATFORM.moduleName('./components/sign-out/sign-out');
+const SignUpForm = PLATFORM.moduleName('./components/sign-up/sign-up');
+const UserDogsList = PLATFORM.moduleName(
   './components/breeder-dogs/breeder-dogs'
 );
-
-const userPairsList = PLATFORM.moduleName(
+const UserLittersList = PLATFORM.moduleName(
+  './components/user-litters-list/user-litters-list'
+);
+const UserPairsList = PLATFORM.moduleName(
   './components/user-pairs-list/user-pairs-list'
 );
 
 export const routeMap: RouteConfig[] = [
   {
-    moduleId: userDogsList,
+    moduleId: UserDogsList,
     name: 'dogs',
     route: ['', 'dogs', 'home'],
     title: 'Your dogs'
   },
   {
-    moduleId: userDogsList,
+    moduleId: UserDogsList,
     name: 'dogs/page',
     route: ['dogs/page/:page'],
     title: 'Your dogs'
   },
   {
-    moduleId: userDogsList,
+    moduleId: UserDogsList,
     name: 'dogs/breed',
     route: ['dogs/breed/:breed/page/:page'],
     title: 'Your dogs'
   },
   {
-    moduleId: userDogsList,
+    moduleId: UserDogsList,
     name: 'dogs/name',
     route: ['dogs/name/:name/page/:page'],
     title: 'Your dogs'
   },
   {
-    moduleId: userDogsList,
+    moduleId: UserDogsList,
     name: 'dogs/breed/name',
     route: ['dogs/breed/:breed/name/:name/page/:page'],
     title: 'Your dogs'
   },
   {
+    moduleId: NewDogForm,
     route: ['dogs/new'],
     name: 'new-dog',
-    title: 'Add new dog',
-    moduleId: PLATFORM.moduleName('./components/new-dog/new-dog')
+    title: 'Add new dog'
   },
   {
-    moduleId: userPairsList,
+    moduleId: UserPairsList,
     name: 'pairs',
     route: ['pairs'],
     title: 'Your pairs'
   },
   {
-    moduleId: userPairsList,
+    moduleId: UserPairsList,
     name: 'pairs/breed',
     route: ['pairs/breed/:breed/page/:page'],
     title: 'Your pairs'
   },
   {
-    moduleId: userPairsList,
+    moduleId: UserPairsList,
     name: 'pairs/breed/name',
     route: ['pairs/breed/:breed/name/:name/page/:page'],
     title: 'Your pairs'
   },
   {
-    moduleId: userPairsList,
+    moduleId: UserPairsList,
     name: 'pairs/name',
     route: ['pairs/name/:name/page/:page'],
     title: 'Your pairs'
   },
   {
-    moduleId: userPairsList,
+    moduleId: UserPairsList,
     name: 'pairs/page',
     route: ['pairs/page/:page'],
     title: 'Your pairs'
   },
   {
-    route: ['pairs/new'],
+    moduleId: NewPairForm,
     name: 'new-pair',
-    title: 'Create new pair',
-    moduleId: PLATFORM.moduleName('./components/new-pair/new-pair')
+    route: ['pairs/new'],
+    title: 'Create new pair'
   },
   {
+    moduleId: UserLittersList,
     name: 'litters',
-    moduleId: userLittersList,
     route: ['litters'],
     title: 'Your litters'
   },
   {
+    moduleId: UserLittersList,
     name: 'litters/breed',
-    moduleId: userLittersList,
     route: ['litters/breed/:breed/page/:page'],
     title: 'Your litters'
   },
   {
+    moduleId: UserLittersList,
     name: 'litters/breed/name',
-    moduleId: userLittersList,
     route: ['litters/breed/breed:/name/:name/page/:page'],
     title: 'Your litters'
   },
   {
+    moduleId: UserLittersList,
     name: 'litters/name',
-    moduleId: userLittersList,
     route: ['litters/name/:name/page/:page'],
     title: 'Your litters'
   },
   {
+    moduleId: UserLittersList,
     name: 'litters/page',
-    moduleId: userLittersList,
     route: ['litters/page/:page'],
     title: 'Your litters'
   },
   {
-    route: ['litters/new'],
+    moduleId: NewLitterForm,
     name: 'new-litter',
-    title: 'Add a new litter',
-    moduleId: PLATFORM.moduleName('./components/new-litter/new-litter')
+    route: ['litters/new'],
+    title: 'Add a new litter'
   },
   {
-    route: ['litters/new/from-pair'],
+    moduleId: NewLitterFromPairForm,
     name: 'new-litter-from-pair',
-    title: 'Add a new litter',
-    moduleId: PLATFORM.moduleName(
-      './components/new-litter-from-pair/new-litter-from-pair'
-    )
+    route: ['litters/new/from-pair'],
+    title: 'Add a new litter'
   },
   {
-    route: ['sign-up', 'register'],
+    moduleId: SignUpForm,
     name: 'sign-up',
-    title: 'Sign Up',
-    moduleId: PLATFORM.moduleName('./components/sign-up/sign-up')
+    route: ['sign-up', 'register'],
+    title: 'Sign Up'
   },
   {
-    route: ['sign-in', 'login'],
+    moduleId: SignInForm,
     name: 'sign-in',
-    title: 'Sign in to Kennelog',
-    moduleId: PLATFORM.moduleName('./components/sign-in/sign-in')
+    route: ['sign-in', 'login'],
+    title: 'Sign in to Kennelog'
   },
   {
-    route: ['sign-out', 'logout'],
+    moduleId: SignOutForm,
     name: 'sign-out',
-    title: 'Signing out?',
-    moduleId: PLATFORM.moduleName('./components/sign-out/sign-out')
+    route: ['sign-out', 'logout'],
+    title: 'Signing out?'
   }
 ];
